48d708b4525de3addcaf25b58c0f6861ebdd07c8
DarkRajeshow2 <itsrajeshadeli@gmail.com>
2024-06-17 11:56:08 +0530

    Added admin and simple screen naviagation.

---
+++ routes/userRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import auth from '../middleware/auth.js'
+import {
+    adminLogin,
+    getUserData,
+    getUserId,
+    isAdminUser,
+    loginUser,
+    logoutUser,
+    registerUser
+} from "../controllers/_User.js";
+
+const router = express.Router();
+
+// /api/user
+router.get("/", getUserData);
+router.get("/id", getUserId);
+router.get("/admin/verify", isAdminUser);
+
+router.post("/admin", adminLogin);
+router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+
+export default router;
+
